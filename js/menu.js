bae16df9db861fed830035e681bae6a10993b08c
stancevic <lukastancevski@proton.me>
2023-12-24 19:41:24 +0100

    added SEO

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,5 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navigacijaLinkovi = document.getElementsByClassName('navigacija-linkovi')[0];
-/*
-toggleButton.addEventListener('click', () => {
-  navigacijaLinkovi.classList.toggle('active');
-})*/
 
 toggleButton.addEventListener('click', funkcijaToggle);
 
